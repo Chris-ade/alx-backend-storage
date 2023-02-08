5386503d29a87756dca69d131d76a29eaa377c1e
Chris Ade <101924502+Chris-ade@users.noreply.github.com>
2023-02-08 23:55:27 +0100

    Update 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- Ranks country origins of bands,
 -- ordered by the number of (non-unique) fans
-SELECT origin, SUM(nb_fans) as nb_fans -- Select the origin and sum of fans for each group of bands with the same origin
+SELECT origin, SUM(fans) as nb_fans -- Select the origin and sum of fans for each group of bands with the same origin
 FROM metal_bands -- Source table containing information on the metal bands
 GROUP BY origin -- Group the bands by their origin
 ORDER BY nb_fans DESC; -- Order the result by the number of fans in descending order
