76e0c210e72531bc2c42aa81b8ced66b4ff25882
Chris Ade <101924502+Chris-ade@users.noreply.github.com>
2023-02-09 00:26:44 +0100

    Update 5-valid_email.sql

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,23 +1,15 @@
--- Create the stored procedure `AddBonus`
+-- SQL script that creates a trigger that resets 
+-- the attribute valid_email only when the email has been changed.
+
 DELIMITER $$
-CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+CREATE TRIGGER reset 
+BEFORE UPDATE
+ON users
+FOR EACH ROW
 BEGIN
-    DECLARE project_id INT;
-
-    -- Check if the project exists
-    SELECT id INTO project_id FROM projects WHERE name = project_name;
-    -- If not, insert a new project
-    IF project_id IS NULL THEN
-        INSERT INTO projects (name) VALUES (project_name);
-        SELECT LAST_INSERT_ID() INTO project_id;
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
     END IF;
-
-    -- Insert the new correction into the `corrections` table
-    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
-
-    -- Update the average score of the user
-    UPDATE users
-    SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id)
-    WHERE id = user_id;
 END $$
-DELIMITER ;
+
+DELIMITER;
