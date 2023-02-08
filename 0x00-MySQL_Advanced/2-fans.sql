80d029fa595563880b127758adcb212e84fc8be4
Chris Ade <101924502+Chris-ade@users.noreply.github.com>
2023-02-08 23:23:38 +0100

    Update 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,14 +1,5 @@
--- Create a table to store the result of the ranking
-CREATE TABLE IF NOT EXISTS country_origin_rank (
-  origin VARCHAR(255) NOT NULL,
-  nb_fans INT NOT NULL
-);
-
--- Clear the contents of the table
-TRUNCATE TABLE country_origin_rank;
-
--- Insert the result of the ranking into the table
-INSERT INTO country_origin_rank (origin, nb_fans)
+-- Ranks country origins of bands, 
+-- ordered by the number of (non-unique) fans
   SELECT origin, SUM(nb_fans) as nb_fans
   FROM metal_bands
   GROUP BY origin
