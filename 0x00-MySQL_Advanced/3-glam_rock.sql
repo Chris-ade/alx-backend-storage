3e7007d7df817f60d190e4766bbbcd1de82a2907
Chris Ade <101924502+Chris-ade@users.noreply.github.com>
2023-02-09 00:00:21 +0100

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,3 @@
 -- Select the band_name and lifespan columns from the metal_bands table
-SELECT band_name, 
-       -- Calculate the lifespan of each band by subtracting their year of formation 
-       -- from the year they split
-       YEAR(split) - YEAR(formed) AS lifespan
-FROM metal_bands
--- Only include bands with Glam Rock as their main style
-WHERE main_style = 'Glam Rock'
--- Order the results by lifespan, from longest to shortest
-ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, 2022) - formed)
+AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
