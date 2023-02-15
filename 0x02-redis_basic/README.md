20d675645cd89e8aca02ee8a46d3a028561f5d1b
Chris Adebiyi <joeade12054@gmail.com>
2023-02-15 13:40:18 -0800

    Task done

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,2 @@
+# 0x02. Redis basic
+Contains tasks on Redis Basics
---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Cache module
+"""
+
+import redis
+import uuid
+
+
+class Cache:
+    """
+    Cache class that interacts with Redis
+    """
+
+    def __init__(self) -> None:
+        """
+        Initialize Cache instance
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: bytes) -> str:
+        """
+        Store the input data in Redis using a randomly generated key and return the key
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
